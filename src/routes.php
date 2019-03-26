6b62ed5d0f5c9da39b8f0e6ab041a089570d7df5
Victor Eliasson <victor@adaptivemedia.se>
2019-03-26 20:19:12 +0100

    Dont load access route if its false

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,10 @@
 <?php
-
-Route::get(config('site-lock.access-url'), function () {
-    session()->put(config('site-lock.session-key'), true);
-    return redirect(config('site-lock.redirect-url'));
-})->middleware('web');
+// Load the access route but only if it is
+// set so we don't load an empty url route
+$accessRoute = config('site-lock.access-url');
+if ($accessRoute) {
+    Route::get(config('site-lock.access-url'), function () {
+        session()->put(config('site-lock.session-key'), true);
+        return redirect(config('site-lock.redirect-url'));
+    })->middleware('web');
+}
